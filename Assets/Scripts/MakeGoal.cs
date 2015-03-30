59cb52c497fbc321393f193476cfd806cd79ba23
Celsuss <Celsuss@hotmail.com>
2015-03-30 13:41:22 +0200

    Playable version

--- Assets/Scripts/MakeGoal.cs
+++ Assets/Scripts/MakeGoal.cs
@@ -3,11 +3,13 @@ using System.Collections;
 
 public class MakeGoal : MonoBehaviour {
 
-	public GameObject PointManager;
+	private GameObject PointManager;
+	//private Component PointManager;
+	public GameObject Owner;
 
 	// Use this for initialization
 	void Start () {
-	
+		PointManager = GameObject.Find ("Manager");//;.GetComponent<PointsManager> ();
 	}
 	
 	// Update is called once per frame
@@ -16,11 +18,31 @@ public class MakeGoal : MonoBehaviour {
 	}
 
 	void OnTriggerEnter2D(Collider2D collision){
-		if (collision.gameObject.tag == "Ball")
+		if (collision.gameObject.tag != "Ball")
+			return;
+
+		GameObject shooter = GetShooter (collision.gameObject).gameObject;
+
+		if(shooter.Equals(Owner))
 			Goal(collision.gameObject);  
 	} 
 
-	void Goal(GameObject ball){
+	Transform GetShooter(GameObject ball){
+		Transform parent;
+
+		if (ball.transform.parent != null)
+			parent = ball.transform.parent;
+		else 
+			parent = ball.GetComponent<GetParent> ().PreviousParent;
+
+		return parent;
+	}
+
+	void Goal(GameObject shooter){
+		PointManager.GetComponent<PointsManager> ().AddPoint (Owner);
+	}
+
+	/*void Goal(GameObject ball){
 		Transform parent;
 		if (ball.transform.parent != null)
 			parent = ball.transform.parent;
@@ -28,5 +50,5 @@ public class MakeGoal : MonoBehaviour {
 			parent = ball.GetComponent<GetParent> ().PreviousParent;
 
 		PointManager.GetComponent<PointsManager> ().AddPoint (parent.gameObject);
-	}
+	}*/
 }
